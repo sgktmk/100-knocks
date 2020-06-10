4cd5f1ab071c1cbf7119c470b9b0a3ea6e3195ee
sgktmk <sgktmk@gmail.com>
2020-06-10 17:02:09 +0900

    add scripting lang

---
+++ basic/java/01_add/addNums.java
@@ -0,0 +1,7 @@
+// 12345+23456を計算して結果を表示するプログラムを作成せよ。
+public class addNums {
+    public static void main(String[] args) {
+        System.out.println("12345 + 23456 = " + (12345 + 23456));
+    }
+}
+
