b30c7e14db654c89235798cb32fbd8c12f1376b4
sgktmk <sgktmk@gmail.com>
2020-06-28 14:23:07 +0900

    6/28

--- basic/java/17/array.java
+++ basic/java/17/array.java
@@ -1,6 +1,10 @@
-// 
+// 要素数10の整数型の配列を宣言し、i番目の要素の初期値をiとし、順に値を表示するプログラムを作成せよ。
 public class array {
     public static void main(String[] args) {
-        System.out.println("Hello Java in Android World!");
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+            System.out.println(nums[i]);
+        }
     }
 }
