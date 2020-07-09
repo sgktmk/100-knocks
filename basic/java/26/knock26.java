33e31041b90826a81e80e5bb1b378d1de7fff4ae
sgktmk <sgktmk@gmail.com>
2020-07-09 22:33:36 +0900

    7/9

--- basic/java/26/knock26.java
+++ basic/java/26/knock26.java
@@ -1,8 +1,26 @@
-//
+// 整数値を入力させ、その値が1ならone、2ならtwo、3ならthree、それ以外ならothersと表示するプログラムをswicth-case文を使って作成せよ。
 import java.util.Scanner;
 
 public class knock26 {
     public static void main(String args[])  {
-        // Later
+        Scanner scan = new Scanner(System.in);
+        System.out.print("input number: ");
+
+        int num = Integer.parseInt(scan.next());
+
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("others");
+                break;
+        }
     }
 }
