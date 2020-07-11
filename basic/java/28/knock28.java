c065f7557bc98bfe3bfd457e829035160282ed87
sgktmk <sgktmk@gmail.com>
2020-07-11 22:01:32 +0900

    ubuntuからのコミットテスト

---
+++ basic/java/28/knock28.java
@@ -0,0 +1,11 @@
+// 整数値を入力させ、その値の階乗を表示するプログラムを作成せよ。ただし、0以下の値を入力した場合は1と表示する。
+import java.util.Scanner;
+
+public class knock28 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("input number: ");
+
+        
+    }
+}
