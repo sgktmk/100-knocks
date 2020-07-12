0e6a7b79f204b36cd169b1c18d43898ac51eb4bc
sgktmk <sgktmk@gmail.com>
2020-07-12 11:20:12 +0900

    7/12

--- basic/java/28/knock28.java
+++ basic/java/28/knock28.java
@@ -4,8 +4,23 @@ import java.util.Scanner;
 public class knock28 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("input number: ");
 
-        
+        System.out.print("input number: ");
+        int number = Integer.parseInt(scan.next());
+
+        System.out.println("factorial: " + factorial(number));
+    }
+    public static int factorial(int inputNumber) {
+        if (inputNumber <= 0) {
+            return 1;
+        }
+
+        int count = 1;
+        int returnNumber = 1;
+        while (count <= inputNumber) {
+            returnNumber = returnNumber * count;
+            count++;
+        }
+        return returnNumber;
     }
 }
