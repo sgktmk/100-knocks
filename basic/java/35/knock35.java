5d4f52588d5145192cf2ed417d9a7a9083e8b78e
sgktmk <sgktmk@gmail.com>
2020-07-24 21:40:49 +0900

    change

--- basic/java/35/knock35.java
+++ basic/java/35/knock35.java
@@ -1,7 +1,11 @@
 // {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、整数値を入力させ、要素番号が入力値である配列要素の値を表示するプログラムを作成せよ。入力値が配列の要素の範囲外であるかどうかのチェックは省略してよい。
+import java.util.Scanner;
 
 public class knock35 {
     public static void main(String args[]) {
-        System.out.println("hello");
+	Scanner scan = new Scanner(System.in);
+        System.out.print("input index: ");
+
+	int index = Integer.parseInt(scan.next());
     }
 }
