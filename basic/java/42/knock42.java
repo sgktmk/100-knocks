b5638bf3ba38494f486e119f2ff7ea8963c6cfec
sgktmk <sgktmk@gmail.com>
2020-08-01 14:34:51 +0900

    8/1

---
+++ basic/java/42/knock42.java
@@ -0,0 +1,8 @@
+// 整数値を3つ入力させ、それらの値が小さい順（等しくてもよい）に並んでいるか判定し、小さい順に並んでいる場合はOK、そうなっていない場合はNGと表示するプログラムを作成せよ。
+import java.util.Scanner;
+
+public class knock42 {
+    public static void main(String args[]) {
+    
+    }
+}
