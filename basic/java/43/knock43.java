fab8848c1fa90f791e01ca730d3ad40f70ff54d0
sgktmk <sgktmk@gmail.com>
2020-08-13 20:04:15 +0900

    8/13

--- basic/java/43/knock43.java
+++ basic/java/43/knock43.java
@@ -1,3 +1,9 @@
 // 2次方程式 ax^2 + bx + c = 0 （x^2はxの2乗の意味）の係数a, b, cを入力し、2次方程式の解が2つの実数解か、重解か、2つの虚数解かを判別するプログラムを作成せよ。
 
 import java.util.Scanner;
+
+public class knock43 {
+    public static void main(String args[]) {
+        System.out.println("test");
+    }
+}
