db4dc0cec0fa79ae337c44322682228cc68c8f04
sgktmk <sgktmk@gmail.com>
2020-08-16 22:07:46 +0900

    8/16

--- basic/java/43/knock43.java
+++ basic/java/43/knock43.java
@@ -6,5 +6,6 @@ public class knock43 {
     public static void main(String args[]) {
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
     }
 }
